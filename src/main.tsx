0ab8409c968de047252a449d61cd4c41706f759f
gicrisf <giovanni.crisalfi@protonmail.com>
2024-11-30 03:11:47 +0100

    It works

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {NextUIProvider} from "@nextui-org/react";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </StrictMode>,
 )
